f8258a02ca477c76dcb9d7c4682b3b715830898c
Marcin Ozga <marcinozga26@gmail.com>
2013-04-05 17:33:18 +0200

    wspolne

--- Ksiazka telefoniczna/WindowsFormsApplication1/Form1.Designer.cs
+++ Ksiazka telefoniczna/WindowsFormsApplication1/Form1.Designer.cs
@@ -71,6 +71,7 @@
             this.groupBox6 = new System.Windows.Forms.GroupBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.edytuj_button = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -467,12 +468,20 @@
             this.edytuj_button.UseVisualStyleBackColor = true;
             this.edytuj_button.Click += new System.EventHandler(this.edytuj_button_Click);
             // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(750, 333);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 60;
+            // 
             // Lista_Kontaktów
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
             this.ClientSize = new System.Drawing.Size(983, 477);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.edytuj_button);
             this.Controls.Add(this.groupBox6);
             this.Controls.Add(this.www_button);
@@ -498,6 +507,7 @@
             this.groupBox6.ResumeLayout(false);
             this.groupBox6.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -546,6 +556,7 @@
         private System.Windows.Forms.GroupBox groupBox6;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button edytuj_button;
+        private System.Windows.Forms.TextBox textBox3;
 
 
     }
